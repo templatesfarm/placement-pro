110e0a8c4d249e2077fe1445e5ff688c51f2b719
DeepakPahawa <deepak.pahawa1911@gmail.com>
2025-01-24 20:52:56 +0530

    Portfolio Completed

---
+++ src/components/portfolio/Projects.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { usePortfolioStore } from "@/store/usePortfolioStore";
+import { useAppStore } from "@/store/appStore";
+import { ProjectsEditableWithAuth } from "portfolioui/placement-pro";
+
+export const Projects = () => {
+  const {
+    saveProjectsInfo,
+    portfolio: { projectsInfo },
+    isLoading,
+  } = usePortfolioStore();
+  const { isEditing } = useAppStore();
+  return (
+    <ProjectsEditableWithAuth
+      isEditing={isEditing}
+      saveProjectsInfo={saveProjectsInfo}
+      projectsInfo={projectsInfo}
+      isLoading={isLoading}
+      circleClassName="bg-blue-300 dark:bg-blue-300"
+      lineClassName="from-violet-800 via-blue-700"
+    />
+  );
+};
